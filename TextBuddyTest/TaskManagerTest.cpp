eff96223ce36e4c7eba2610e644db4fe0b6a25c4
jin <jin@crypt.sg>
2014-09-16 03:54:13 +0800

    More tests for CRUD.

--- TextBuddyTest/TaskManagerTest.cpp
+++ TextBuddyTest/TaskManagerTest.cpp
@@ -1,5 +1,7 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
+#include <iostream>
+#include <fstream>
 
 #include "..\TextBuddy\TaskManager.h"
 
@@ -10,13 +12,46 @@ namespace TextBuddyTest
 	TEST_CLASS(TextBuddyTest)
 	{
 	public:
-		
-		TEST_METHOD(Add)
+
+		TEST_METHOD(LoadEmptyTasksFile)
 		{
 			TaskManager taskManager = TaskManager("tasks.txt");
-			int current_size = taskManager.numberOfTasks();
+			Assert::AreEqual(0, taskManager.numberOfTasks());
+		}
+
+		TEST_METHOD(AddTask)
+		{
+			TaskManager taskManager = TaskManager("tasks.txt");
 			taskManager.executeCommand("add buy milk");
-			Assert::AreEqual(current_size + 1, taskManager.numberOfTasks());
+			Assert::AreEqual(1, taskManager.numberOfTasks());
+		}
+
+
+		TEST_METHOD(LoadNonEmptyTasksFile)
+		{
+			// Adding a task manually to the file
+			std::ofstream out("tasks.txt");
+			out << "add buy milk\n";
+			out.close();
+
+			TaskManager taskManager = TaskManager("tasks.txt");
+			Assert::AreEqual(1, taskManager.numberOfTasks());
+		}
+
+
+		TEST_METHOD(ClearTasks)
+		{
+			TaskManager taskManager = TaskManager("tasks.txt");
+			taskManager.executeCommand("clear");
+			taskManager.executeCommand("add buy milk");
+			taskManager.executeCommand("add walk the dog");
+			taskManager.executeCommand("clear");
+			Assert::AreEqual(0, taskManager.numberOfTasks());
+		}
+
+		TEST_METHOD_CLEANUP(RemoveTasksFile)
+		{
+			std::remove("tasks.txt");
 		}
 
 	};
